b9d07dc728039c43642c5e028b437849a53cf4c1
Kreusna Bayu <bayusans235@gmail.com>
2024-07-11 00:48:00 +0700

    first commit & buil Home and About

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Kreusna Bayu </span>
+            from <span className="purple"> Bandung, Indonesai.</span>
+            <br />
+            I am currently employed as a mobile apps developer at PT Eksam Digital Edukasi.
+            <br />
+            I am a student at Institut Teknologi Nasional Bandung.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Football
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "The way to get started is to quit talking and begin doing!!!"{" "}
+          </p>
+          <footer className="blockquote-footer">Walt Disney</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            Programming has captured my heart, and I’ve picked up quite a few things along the way 🧑🏻‍💻
+              <br />
+              <br />I am proficient in languages like
+              <i>
+                <b className="purple"> C++, Python, JavaScript, and Flutter. </b>
+              </i>
+              <br />
+              <br />
+              My interests lie in crafting cutting-edge &nbsp;
+              <i>
+                <b className="purple">Web and mobile applications,  </b> with a special focus on {" "}
+                <b className="purple">
+                IoT and machine learning innovations.
+                </b>
+              </i>
+              <br />
+              <br />
+              I love leveraging my skills to build impactful products, whether it's through <b className="purple">Flutter for mobile development </b> or
+              <i>
+                <b className="purple">
+                  {" "}
+                  for web applications
+                </b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="purple"> React.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/KreusnaBayu"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+            
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/kreusna-bayu-3b569528b/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/bayuprangga/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
