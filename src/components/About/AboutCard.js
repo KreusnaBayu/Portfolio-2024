04b450bf7b43e576a5669b32bae4c90401e64afd
Kreusna Bayu <bayusans235@gmail.com>
2024-07-17 22:56:06 +0700

    Change Avatar

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Kreusna Bayu </span>
-            from <span className="purple"> Bandung, Indonesai.</span>
+            from <span className="purple"> Bandung, Indonesia.</span>
             <br />
             I am currently employed as a mobile apps developer at PT Eksam Digital Edukasi.
             <br />
